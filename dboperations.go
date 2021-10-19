e03738be7c8e8b9cc9a1c1dc370d4345deb1cca2
jonathanheaden <jonathan.headen@outlook.com>
2021-10-19 18:49:31 +1100

    get store details from db

---
+++ dboperations.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+	
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	// "go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func getdatabases(client *mongo.Client) ([]string,error) {
+	var dblist []string
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	dblist, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Warn(err)
+		return dblist, err
+	}
+	return dblist, nil
+}
+
+func getstoretoken(storename string, client *mongo.Client) string {
+	fmt.Printf("token for %s", storename)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	var doc bson.M
+	collection := client.Database("etync").Collection("shops")
+	filter := bson.D{{"shopify_domain", storename}}
+	if err := collection.FindOne(ctx, filter).Decode(&doc); err != nil {
+		log.Warn(err)
+		return ""
+	}
+	return fmt.Sprintf("%v",doc["accessToken"])
+}
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+  
   log "github.com/sirupsen/logrus"
+  // "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 )
 
@@ -11,5 +17,19 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
-	fmt.Println(config.MONGO_URI)
+	client, err := mongo.NewClient(options.Client().ApplyURI(config.MONGO_URI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer client.Disconnect(ctx)
+  databaselist, err := getdatabases(client)
+ 
+	fmt.Println(databaselist)
+  fmt.Printf("token %s", getstoretoken("etsync.myshopify.com", client))
 }
