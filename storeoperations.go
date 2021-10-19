b9c0b3cb1704bfe579c7b47d24219a4ce178d6f7
jonathanheaden <jonathan.headen@outlook.com>
2021-10-19 19:48:08 +1100

    add the graphql status check skeleton

--- storeoperations.go
+++ storeoperations.go
@@ -6,7 +6,9 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
+
 type BulkRequest struct {
 	Data struct {
 		BulkOperationRunQuery struct {
@@ -19,7 +21,36 @@ type BulkRequest struct {
 	} `json:"data"`
 }
 
+type BulkRequestStatus struct {
+	Data struct {
+		CurrentBulkOperation struct {
+			ID             string      `json:"id"`
+			Status         string      `json:"status"`
+			ErrorCode      interface{} `json:"errorCode"`
+			CreatedAt      time.Time   `json:"createdAt"`
+			CompletedAt    time.Time   `json:"completedAt"`
+			ObjectCount    string      `json:"objectCount"`
+			FileSize       string      `json:"fileSize"`
+			URL            string      `json:"url"`
+			PartialDataURL interface{} `json:"partialDataUrl"`
+		} `json:"currentBulkOperation"`
+	} `json:"data"`
+	Extensions struct {
+		Cost struct {
+			RequestedQueryCost int `json:"requestedQueryCost"`
+			ActualQueryCost    int `json:"actualQueryCost"`
+			ThrottleStatus     struct {
+				MaximumAvailable   float64 `json:"maximumAvailable"`
+				CurrentlyAvailable int     `json:"currentlyAvailable"`
+				RestoreRate        float64 `json:"restoreRate"`
+			} `json:"throttleStatus"`
+		} `json:"cost"`
+	} `json:"extensions"`
+}
+
+
 func registerbulkquery(storeurl, token string) (string, error) {
+	var response BulkRequest 
 	url := fmt.Sprintf("https://%s/admin/api/2021-01/graphql.json", storeurl)
 	method := "POST"
 
@@ -40,20 +71,58 @@ func registerbulkquery(storeurl, token string) (string, error) {
 		fmt.Println(err)
 		return "",err
 	}
+
 	defer res.Body.Close()
-	var response BulkRequest 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
 		return "",err
 	}
+
 	if err := json.Unmarshal(body, &response); err != nil {
         return "",err
     }
+
 	if response.Data.BulkOperationRunQuery.BulkOperation.Status == "CREATED" {
 		return response.Data.BulkOperationRunQuery.BulkOperation.ID, nil
 	} else {
 		errstring := strings.Join(response.Data.BulkOperationRunQuery.UserErrors,"\n")
 		return "", fmt.Errorf("%s",errstring)
 	}
+}
+
+func getBulkRequestStatus(storeurl, token string){
+	var response BulkRequestStatus
+	url := fmt.Sprintf("https://%s/admin/api/2021-01/graphql.json", storeurl)
+	method := "POST"
+  
+	payload := strings.NewReader("{\"query\":\"query {\\n  currentBulkOperation {\\n    id\\n    status\\n    errorCode\\n    createdAt\\n    completedAt\\n    objectCount\\n    fileSize\\n    url\\n    partialDataUrl\\n  }\\n}\\n\",\"variables\":{}}")
+  
+	client := &http.Client {
+	}
+	req, err := http.NewRequest(method, url, payload)
+  
+	if err != nil {
+	  fmt.Println(err)
+	  return
+	}
+	req.Header.Add("X-Shopify-Access-Token", token)
+	req.Header.Add("Content-Type", "application/json")
+  
+	res, err := client.Do(req)
+	if err != nil {
+	  fmt.Println(err)
+	  return
+	}
+	defer res.Body.Close()
+  
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+	  fmt.Println(err)
+	  return
+	}
+	if err := json.Unmarshal(body, &response); err != nil {
+        return 
+    }
+	fmt.Println(response.Data.CurrentBulkOperation.Status)
 }
