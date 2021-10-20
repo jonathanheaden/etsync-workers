2cb604b80f750cb97de71162a9649429cda7582b
jonathanheaden <jonathan.headen@outlook.com>
2021-10-20 20:56:55 +1100

    add logging and shopify graphql funcs

--- storeoperations.go
+++ storeoperations.go
@@ -7,6 +7,8 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 type BulkRequest struct {
@@ -48,81 +50,153 @@ type BulkRequestStatus struct {
 	} `json:"extensions"`
 }
 
-
-func registerbulkquery(storeurl, token string) (string, error) {
-	var response BulkRequest 
+func registerbulkquery(storeurl, token, query string,) (string, error) {
+	var response BulkRequest
 	url := fmt.Sprintf("https://%s/admin/api/2021-01/graphql.json", storeurl)
+	log.Info(fmt.Sprintf("sending request to %s", url))
 	method := "POST"
-
-	payload := strings.NewReader("{\"query\":\"mutation {\\n  bulkOperationRunQuery(\\n   query: \\\"\\\"\\\"\\n   {\\n  inventoryItems {\\n    edges {\\n      node {\\n        id\\n        inventoryLevels {\\n          edges {\\n            node {\\n              location {\\n                address {\\n                  address1\\n                  address2\\n                  city\\n                  country\\n                }\\n                id\\n              }\\n              available\\n              id\\n              updatedAt\\n            }\\n\\n          }\\n        }\\n      }\\n    }\\n  }\\n  }\\n    \\\"\\\"\\\"\\n  ) {\\n    bulkOperation {\\n      id\\n      status\\n    }\\n    userErrors {\\n      field\\n      message\\n    }\\n  }\\n}\",\"variables\":{}}")
-
+	payload := strings.NewReader(query)
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, payload)
 
 	if err != nil {
-		fmt.Println(err)
-		return "",err
+		log.Errorf("Error with http client: %v",err)
+		return "", err
 	}
 	req.Header.Add("X-Shopify-Access-Token", token)
 	req.Header.Add("Content-Type", "application/json")
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return "",err
+		log.Errorf("Error with http client request action: %v",err)
+		return "", err
 	}
+	log.Info(fmt.Sprintf("response code %d",res.StatusCode))
 
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err)
-		return "",err
+		log.Errorf("Error reading response body: %v",err)
+		return "", err
 	}
 
 	if err := json.Unmarshal(body, &response); err != nil {
-        return "",err
-    }
+		log.Errorf("Error with response unmarshall: %v",err)
+		return "", err
+	}
 
+	log.WithFields(log.Fields{
+		"Status": response.Data.BulkOperationRunQuery.BulkOperation.Status,
+		"ID": response.Data.BulkOperationRunQuery.BulkOperation.ID,
+	}).Info("Response from Shopify Register Bulk Query operation")
+	
 	if response.Data.BulkOperationRunQuery.BulkOperation.Status == "CREATED" {
 		return response.Data.BulkOperationRunQuery.BulkOperation.ID, nil
 	} else {
-		errstring := strings.Join(response.Data.BulkOperationRunQuery.UserErrors,"\n")
-		return "", fmt.Errorf("%s",errstring)
+		errstring := strings.Join(response.Data.BulkOperationRunQuery.UserErrors, "\n")
+		log.Warnf("Errors contained in response: %v",err)
+		return "", fmt.Errorf("%s", errstring)
 	}
 }
 
-func getBulkRequestStatus(storeurl, token string){
+func getBulkRequestStatus(storeurl, token string) (string, string, error) {
 	var response BulkRequestStatus
 	url := fmt.Sprintf("https://%s/admin/api/2021-01/graphql.json", storeurl)
 	method := "POST"
-  
+
 	payload := strings.NewReader("{\"query\":\"query {\\n  currentBulkOperation {\\n    id\\n    status\\n    errorCode\\n    createdAt\\n    completedAt\\n    objectCount\\n    fileSize\\n    url\\n    partialDataUrl\\n  }\\n}\\n\",\"variables\":{}}")
-  
-	client := &http.Client {
-	}
+
+	client := &http.Client{}
 	req, err := http.NewRequest(method, url, payload)
-  
 	if err != nil {
-	  fmt.Println(err)
-	  return
+		log.Errorf("Error with http client: %v",err)
+		return "","", err
 	}
 	req.Header.Add("X-Shopify-Access-Token", token)
 	req.Header.Add("Content-Type", "application/json")
-  
+
 	res, err := client.Do(req)
 	if err != nil {
-	  fmt.Println(err)
-	  return
+		fmt.Println(err)
+		return "","", err
 	}
 	defer res.Body.Close()
-  
+
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-	  fmt.Println(err)
-	  return
+		log.Errorf("Error reading response body: %v",err)
+		return "","", err
 	}
 	if err := json.Unmarshal(body, &response); err != nil {
-        return 
-    }
-	fmt.Println(response.Data.CurrentBulkOperation.Status)
+		return "","", err
+	}
+	return response.Data.CurrentBulkOperation.Status, response.Data.CurrentBulkOperation.URL, nil
+}
+
+func getproductvariants(storeurl, token string) (string, error) {
+	query := "{\"query\":\"mutation {\\n  bulkOperationRunQuery(\\n   query: \\\"\\\"\\\"\\n    {\\n      products {\\n        edges {\\n          node {\\n            id,\\n            variants {\\n              edges {\\n                node {\\n                  displayName,\\n                  id,\\n                  inventoryManagement,\\n                  inventoryItem  {\\n                     id\\n                  },\\n                  product {\\n                    id,\\n                    title\\n                  }\\n                }\\n              }\\n            }\\n          }\\n        }\\n      }\\n    }\\n    \\\"\\\"\\\"\\n  ) {\\n    bulkOperation {\\n      id\\n      status\\n    }\\n    userErrors {\\n      field\\n      message\\n    }\\n  }\\n}\",\"variables\":{}}"
+	_,err := registerbulkquery(storeurl, token, query)
+	if err != nil {
+		return "",err
+	}
+	var url string
+	attempt := 1
+	statusurl := "PENDING"
+	for statusurl != "COMPLETED" {
+		statusurl, url, err = getBulkRequestStatus(storeurl, token)
+		if err != nil {
+			return "", err
+		}
+		log.WithFields(log.Fields{
+			"Status": statusurl,
+			"retries": attempt,
+		}).Info()
+		// if the statusurl is returned then we should break out before the timed sleep
+		attempt = attempt + 1
+		if attempt > 12 {
+			log.Error("Exiting function as 4 minutes have expired")
+			return "",fmt.Errorf("Query exceeded 4 minute timeout")
+		}
+		time.Sleep(20 * time.Second)
+	}
+	return url, nil
+}
+
+func getinventorylevels(storeurl, token string) (string, error) {
+	query := "{\"query\":\"mutation {\\n  bulkOperationRunQuery(\\n   query: \\\"\\\"\\\"\\n   {\\n  inventoryItems {\\n    edges {\\n      node {\\n        id\\n        inventoryLevels {\\n          edges {\\n            node {\\n              location {\\n                address {\\n                  address1\\n                  address2\\n                  city\\n                  country\\n                }\\n                id\\n              }\\n              available\\n              id\\n              updatedAt\\n            }\\n\\n          }\\n        }\\n      }\\n    }\\n  }\\n  }\\n    \\\"\\\"\\\"\\n  ) {\\n    bulkOperation {\\n      id\\n      status\\n    }\\n    userErrors {\\n      field\\n      message\\n    }\\n  }\\n}\",\"variables\":{}}"
+	_,err := registerbulkquery(storeurl, token, query)
+	if err != nil {
+		return "",err
+	}
+	var url string
+	attempt := 1
+	statusurl := "PENDING"
+	for statusurl != "COMPLETED" {
+		statusurl, url, err = getBulkRequestStatus(storeurl, token)
+		if err != nil {
+			return "", err
+		}
+		log.WithFields(log.Fields{
+			"Status": statusurl,
+			"retries": attempt,
+		}).Info()
+		// if the statusurl is returned then we should break out before the timed sleep
+		attempt = attempt + 1
+		if attempt > 12 {
+			log.Error("Exiting function as 4 minutes have expired")
+			return "",fmt.Errorf("Query exceeded 4 minute timeout")
+		}
+		time.Sleep(20 * time.Second)
+	}
+	return url, nil
+}
+
+func processinventorylevels(url, storename string)  error {
+	// download the JSONL file, parse and update the database
+	return nil
+}
+
+func processproductlevels(url, storename string)  error {
+	// download the JSONL file, parse and update the database
+	return nil
 }
