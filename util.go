f04ce1cb9fedbc469de3eab0aeaadee4305c1e90
jonathanheaden <jonathan.headen@outlook.com>
2021-10-23 08:23:17 +1100

    Add shopname to config

--- util.go
+++ util.go
@@ -9,6 +9,7 @@ type Config struct {
 	ATLAS_MONGO_URI string `mapstructure:"ATLAS_MONGO_URI"`
 	ETSY_CLIENT_ID  string `mapstructure:"ETSY_CLIENT_ID"`
 	APP_ENV         string `mapstructure:"APP_ENV"`
+	SHOP_NAME       string `mapstructure:"SHOP_NAME"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
