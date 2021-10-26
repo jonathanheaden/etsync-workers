8243f42d4aaa8689f8db7f787aa751fca85f28d4
jonathanheaden <jonathan.headen@outlook.com>
2021-10-26 20:13:11 +1100

    Add etsy redirecto uri to the config

--- util.go
+++ util.go
@@ -5,11 +5,12 @@ import (
 )
 
 type Config struct {
-	MONGO_URI       string `mapstructure:"MONGO_URI"`
-	ATLAS_MONGO_URI string `mapstructure:"ATLAS_MONGO_URI"`
-	ETSY_CLIENT_ID  string `mapstructure:"ETSY_CLIENT_ID"`
-	APP_ENV         string `mapstructure:"APP_ENV"`
-	SHOP_NAME       string `mapstructure:"SHOP_NAME"`
+	MONGO_URI         string `mapstructure:"MONGO_URI"`
+	ATLAS_MONGO_URI   string `mapstructure:"ATLAS_MONGO_URI"`
+	ETSY_CLIENT_ID    string `mapstructure:"ETSY_CLIENT_ID"`
+	ETSY_REDIRECT_URI string `mapstructure:"ETSY_REDIRECT_URI"`
+	APP_ENV           string `mapstructure:"APP_ENV"`
+	SHOP_NAME         string `mapstructure:"SHOP_NAME"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
